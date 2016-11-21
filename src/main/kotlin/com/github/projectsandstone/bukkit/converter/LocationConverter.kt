9ffaa656f6e5030a3bddde13dde90029a246a21d
Jonathan <joniweb01@gmail.com>
2016-11-21 18:58:31 -0200

    Readme

---
+++ src/main/kotlin/com/github/projectsandstone/bukkit/converter/LocationConverter.kt
@@ -0,0 +1,4 @@
+package com.github.projectsandstone.bukkit.converter
+
+object LocationConverter {
+}
---
+++ src/test/kotlin/KTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+
+class KTest {
+    @Test
+    fun test() {
+        System.out.println(Unit::class)
+        System.out.println(Void::class)
+    }
+}
